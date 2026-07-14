e6ead17126cf59add1a304272d3dce36ec40c3c1
Saish Pable <pablesaish07@gmail.com>
2026-07-14 09:50:21 +0530

    Time: 1 ms (100%), Space: 47.4 MB (27.07%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int maxCol[] = new int[col];
+
+        for (int j = 0; j < col; j++) {
+            int max = -1;
+            for (int i = 0; i < row; i++) {
+                max = Math.max(max, matrix[i][j]);
+            }
+            maxCol[j] = max;
+        }
+        int answer[][] = new int[row][col];
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(matrix[i][j] == -1 ) {
+                    answer[i][j] = maxCol[j];
+                } else {
+                    answer[i][j] = matrix[i][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
