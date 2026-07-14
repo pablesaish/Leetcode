df498823932dd66c7917d3207f0318b87ed0044e
Saish Pable <pablesaish07@gmail.com>
2026-07-14 16:13:43 +0530

    Time: 1 ms (99.97%), Space: 77.2 MB (48.51%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int result = nums[0];
+        int maxSum = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            int currSum = 0;
+            maxSum = Math.max(maxSum + nums[i], nums[i]);
+            result = Math.max(result, maxSum);
+        }
+        return result;
+    }
+}
