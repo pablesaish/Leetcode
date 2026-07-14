8a7f8f597a79a9e11f08bfdcbb2b83547447e3c9
Saish Pable <pablesaish07@gmail.com>
2026-07-14 12:29:47 +0530

    Time: 2 ms (94.6%), Space: 46.3 MB (71.08%) - LeetHub

--- 3248-snake-in-matrix/3248-snake-in-matrix.java
+++ 3248-snake-in-matrix/3248-snake-in-matrix.java
@@ -2,24 +2,12 @@ class Solution {
     public int finalPositionOfSnake(int n, List<String> commands) {
         int row = 0, col = 0;
         for(int i=0; i<commands.size(); i++) {
-            switch (commands.get(i)) {
-                case "UP":
-                    row--;
-                    break;
-                case "DOWN":
-                    row++;
-                    break;
-                case "RIGHT":
-                    col++;
-                    break;
-                case "LEFT":
-                    col--;
-                    break;
-                default:
-                    break;
-            }
+            String s = commands.get(i);
+            if(s.equals("UP")) row--;
+            else if(s.equals("DOWN")) row++;
+            else if(s.equals("RIGHT")) col++;
+            else col--;
         }
-
-        return row * n + col;
+        return (row * n) + col;
     }
 }
